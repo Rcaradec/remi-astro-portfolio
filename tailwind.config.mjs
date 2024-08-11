d117ad409bb8ebb2cfb7f6733cae95093dfe70fb
Rcaradec <remi.caradec@matou-technologies.fr>
2024-08-11 15:19:14 +0200

    prettier update double quotes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,53 +1,53 @@
 const {
   default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette');
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
-        'slide-logos': 'slide-logos 60s linear infinite',
+        "slide-logos": "slide-logos 60s linear infinite",
       },
       keyframes: {
-        'slide-logos': {
-          to: { transform: 'translateX(calc(-50% - 2rem))' },
+        "slide-logos": {
+          to: { transform: "translateX(calc(-50% - 2rem))" },
         },
         aurora: {
           from: {
-            backgroundPosition: '50% 50%, 50% 50%',
+            backgroundPosition: "50% 50%, 50% 50%",
           },
           to: {
-            backgroundPosition: '350% 50%, 350% 50%',
+            backgroundPosition: "350% 50%, 350% 50%",
           },
         },
       },
       colors: {
-        background: 'hsl(var(--background))',
-        accent: 'hsl(var(--accent))',
-        foreground: 'hsl(var(--foreground))',
-        'muted-foreground': 'hsl(var(--muted-foreground))',
-        border: 'hsl(var(--border))',
-        card: 'hsl(var(--card))',
+        background: "hsl(var(--background))",
+        accent: "hsl(var(--accent))",
+        foreground: "hsl(var(--foreground))",
+        "muted-foreground": "hsl(var(--muted-foreground))",
+        border: "hsl(var(--border))",
+        card: "hsl(var(--card))",
       },
     },
     dark: {
-      background: 'black',
-      color: 'white',
+      background: "black",
+      color: "white",
     },
   },
   plugins: [addVariablesForColors],
 };
 
 function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme('colors'));
+  let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
   addBase({
-    ':root': newVars,
+    ":root": newVars,
   });
 }
