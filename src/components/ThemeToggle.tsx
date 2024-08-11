884dcc4a18bebe8258199f0c00f026904dcb07c2
Rcaradec <remi.caradec@matou-technologies.fr>
2024-08-11 15:08:35 +0200

    dark mode

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  //? Fallback Initial State
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
+
+  //? Addressing the Client/Server Mismatch
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const handleClick = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", newTheme);
+    }
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <button onClick={handleClick}>
+      <img src="/assets/switch.svg" alt="Theme Icon" width="24" height="24" />
+    </button>
+  );
+}
