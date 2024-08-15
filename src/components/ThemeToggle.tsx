80f620dee09533f173213613a8d5b2216e889b3b
Rcaradec <remi.caradec@matou-technologies.fr>
2024-08-15 16:40:13 +0200

    recommandtions / background/ content mods

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -30,12 +30,21 @@ export default function ThemeToggle() {
   }
 
   return (
-    <button onClick={handleClick} id="theme-toggle">
-      {theme === "dark" ? (
-        <img src="/assets/sun.svg" alt="Theme Icon" width="24" height="24" />
-      ) : (
-        <img src="/assets/moon.svg" alt="Theme Icon" width="24" height="24" />
-      )}
+    <button
+      onClick={handleClick}
+      id="theme-toggle"
+      className="p-2  transition-colors duration-300"
+    >
+      <svg
+        fill={theme === "dark" ? "white" : "#00ffff"}
+        height="24px"
+        width="24px"
+        viewBox="0 0 60 60"
+        xmlns="http://www.w3.org/2000/svg"
+        className="transition-colors duration-300"
+      >
+        <path d="M42,12H18C8.075,12,0,20.075,0,30s8.075,18,18,18h24c9.925,0,18-8.075,18-18S51.925,12,42,12z M18,38c0,0.553-0.447,1-1,1 s-1-0.447-1-1V22c0-0.553,0.447-1,1-1s1,0.447,1,1V38z M42,43c-7.168,0-13-5.832-13-13s5.832-13,13-13s13,5.832,13,13 S49.168,43,42,43z" />
+      </svg>
     </button>
   );
 }
